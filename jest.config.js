f7b29d5f5b10c505934f7b8a85d28d91ead012f7
uxthi <thiago@spellbook.com.br>
2023-02-02 00:27:54 -0300

    Fix install deps action

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const config = {
   verbose: true,
+  setupFilesAfterEnv: ["./jest.setup.js"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
