fdd99c307adb9d3aefead11f905eeb2e85d66403
uxthi <thiago@spellbook.com.br>
2023-02-03 10:57:05 -0300

    test/spiner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "./styles";
 
 const Spinner: React.FC = () => {
-  return <Container />;
+  return <Container data-testid="spinner" />;
 };
 
 export default Spinner;
